4aaea864c785e4937b1a1f78ab8996e8c2e36184
alsa07007 <alsa07007@E-PW05CGR5>
2024-11-13 08:35:03 +0100

    Add project files.

---
+++ Uppgift 3.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift_3._2
+{
+    class Program
+    {
+        void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? (j/n)");
+            bool gymnasie = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Ange din ålder:");
+            int ålder = int.Parse(Console.ReadLine());
+            if (gymnasie = true && ålder < 22 && ålder > 18)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig!");
+            }
+            else Console.WriteLine("Vi letar tyvärr efter annan personal just nu.");
+        }
+    }
+}
